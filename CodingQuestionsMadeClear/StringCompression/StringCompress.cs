4feaaae5b0ab2a06e737798f6395dd3c8e109a8b
Pepe <arturo@hotmail.com>
2024-01-21 12:12:23 -0600

    f c

---
+++ CodingQuestionsMadeClear/StringCompression/StringCompress.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+class StringCompress
+{
+
+
+    public static string compress(string input)
+    {
+        // validate before 
+        char current = input[0];
+        int count = 0;
+        StringBuilder output = new StringBuilder();
+
+        foreach (char dot in input)
+        {
+
+            if (dot == current)
+            {
+                count++;
+            }
+            else
+            {
+
+                output.Append(current);
+                output.Append(count);
+                //update current and count
+                current = dot;
+                count = 1;
+
+            }
+
+        }
+
+        output.Append(current);
+        output.Append(count);
+
+
+        return output.ToString();
+
+    }
+
+}
